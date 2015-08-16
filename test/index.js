587dd07e4664a5edae0dda65f32911859d7c4330
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-16 16:54:08 -0700

    Got Chai working.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 let should = require("should");
+let {assert, expect} = require("chai");
 
 import core, {bar, zoo} from '../src/index';
 
 describe("Test core", () => {
   it("should return foo", () => {
+    expect(true).to.equal(true);
+    assert(3, 3, 'Three is 3');
     core().should.equal("foo");
   });
 });
